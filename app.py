c26b7b307370944884356feda01972fbdcd258ba
rabi-m <capital861@gmail.com>
2025-01-04 06:56:00 +0200

    modified app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def product_detail(product_id):
     return render_template('product.html', product=product)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=4000)
